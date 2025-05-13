c4903f57a942cb45aa18139e5cb49adddae3359e
Amuda200 <159070966+Amuda200@users.noreply.github.com>
2025-05-13 07:00:57 +0100

    Update Withdraw.tsx

--- src/pages/Withdraw.tsx
+++ src/pages/Withdraw.tsx
@@ -73,7 +73,7 @@ const Withdraw = () => {
     }
 
     // Validate BPC code
-    if (bpcCode !== "BPC526251") {
+    if (bpcCode !== "BPC024763") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
